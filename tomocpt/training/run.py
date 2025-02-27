d3767475283cf81b9aa67abc98fb0262a97527ae
shahpnmlab <34034134+shahpnmlab@users.noreply.github.com>
2025-02-27 01:25:08 +0000

    didnt work, removed prog bar

--- tomocpt/training/run.py
+++ tomocpt/training/run.py
@@ -212,7 +212,7 @@ def train(
         gradient_clip_val=1.0,
         gradient_clip_algorithm="norm",
         enable_model_summary=False,
-        verbose=False,
+        enable_progress_bar=False
     )
 
     if trainer.is_global_zero:
